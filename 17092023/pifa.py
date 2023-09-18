85ab44e606b318a9a97619d5d9fe8da0179c835b
bodunov78 <ilya.bodunov@gmail.com>
2023-09-18 03:59:25 +0300

    final3 commit

--- 17092023/pifa.py
+++ 17092023/pifa.py
@@ -118,10 +118,36 @@ def fu5():
     print(i, maxi)
 
 
+def fu6():
+    i = 0
+    cnt = 0
+    a = 0
+    maxi = 0
+    while cnt < 100:
+        a += 1
+        b = a
+        while cnt < 100 and b < 323:  # 1201
+            b += 1
+            c = b + a % 2
+            while cnt < 100 and a ** 2 + b ** 2 >= c ** 2:
+
+                i += 1
+                if a ** 2 + b ** 2 == c ** 2:
+                    cnt += 1
+                    print(a, b, c, cnt)
+                    # maxi = max(maxi, b)
+                    break
+                # elif a ** 2 + b ** 2 < c ** 2:
+                #     break
+                c += 2
+    print(i, maxi)
+
+
 # fu()
 # fu0()
 # fu1()
 # fu2()
-fu3()
-fu4()
+# fu3()
+# fu4()
 fu5()
+fu6()
