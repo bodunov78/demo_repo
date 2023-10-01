76c4f7c9c8428898f42fcc8c8c8916e67e02527d
ILYA BODUNOV <airfox@mail.ru>
2023-10-02 00:32:34 +0300

    02.10

--- 30092023/01102023.py
+++ 30092023/01102023.py
@@ -17,3 +17,30 @@ a[1]=10
 print(sorted(a),a,b)
 a.sort(reverse=1)
 print(a)
+
+# различие переменных и списков
+#
+# c=1
+# d=c
+# # c=4
+# print (c,d,id(c),id(d))
+#
+# m=[1,2,3]
+# n=m
+# n[1]=8
+# print (m,n,id(m),id(n))
+
+x=a.pop(1)
+print (a,x)
+# a.reverse()
+a=a[::-1]
+print(a)
+
+for i,x in enumerate(a):
+    if 10<=x<=99:
+        print (i,x)
+        a[i]="!"
+
+print(a)
+m=list(set(a))
+print (type(a),type(m),a,m)
