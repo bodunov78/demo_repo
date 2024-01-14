b994b9f27c915118a36f9d2c473b8a99f2b8b320
ILYA BODUNOV <airfox@mail.ru>
2024-01-15 02:26:28 +0300

    15.01.2024 #1

---
+++ 15012024/8_2.py
@@ -0,0 +1,26 @@
+from itertools import *
+from time import *
+
+#Тип 8 26982
+
+st=time()
+ch0=(0,2,4,6,8,)
+ch=(2,4,6,8)
+nch=(1,3,5,7,9)
+
+d=0
+kk=(0,1,2,3,4,5,6,7,8,9)
+
+count = 0
+for x in range(10000000,100000000,5):
+    m=set(str(x))
+    var=[]
+    if len(m)==8 and str(x)[0]!='0':
+        var=[int(k) for k in str(x)]
+
+
+        if (var[0]%2==var[2]%2==var[4]%2==var[6]%2) and(var[1]%2==var[3]%2==var[5]%2==var[7]%2):
+            count+=1
+            # print(var,count,m)
+
+print(count,time()-st)
