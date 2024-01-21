fbb3afc17eb9c5805f98280f0276519df61de508
ILYA BODUNOV <airfox@mail.ru>
2024-01-22 00:19:00 +0300

    22.01.2024 #1

--- 18012024_kurs/7.py
+++ 18012024_kurs/7.py
@@ -16,5 +16,38 @@ for a in x:
     k = [a[:i] + a[i + 1:] + [a[i] + 1, a[i] + 1] for i in range(len(a))]
 #a = set(tuple(x) for x in k)
     print (k)
+def tre(x):
+    # x={}
+    #x={(2,3,3,3),(2,2,4,4)}
+    # print(type(x))
+    #x.add((3,))
+    nx=set()
+    for a in x:
+        n=list(a)
+        k = tuple()
+        for i,v in enumerate(n):
+            k=tuple(n[:i])+tuple(n[i+1:]) +(n[i]+1,)+(n[i]+1,)
+            nx.add(k)
+            k=()
+
+    # print(nx)
+
+    x=nx
+    # print(x)
+    return x
+        # x.remove(a)
 
 
+        # a.add((j+1,j+1))
+        # print(a,j)
+x={(1,3)}
+
+for i in range(4):
+    x=tre(x)
+    print (x)
+
+m=(1,2,3)
+mn=tuple([x+1,x+1] if x==1 else x for x in m)
+
+
+print ("MN",*mn)
