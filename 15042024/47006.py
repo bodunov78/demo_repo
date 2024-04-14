8722b14c015ab2319ad3af04e9ce8e1a54d7e800
ILYA BODUNOV <airfox@mail.ru>
2024-04-15 01:25:54 +0300

    15.04.2024 #1

---
+++ 15042024/47006.py
@@ -0,0 +1,13 @@
+from itertools import *
+
+cnt = 0
+with open("47006.txt") as f:
+    for s in f:
+        m = list(map(int, s.split()))
+        a = [list(x) for x in combinations(m, 3)]
+
+        c = [(sum(x) - max(x) > max(x)) for x in a]
+        if all(c):
+            cnt += 1
+          # SSS
+    print(cnt)
