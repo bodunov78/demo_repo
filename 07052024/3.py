5e4feda76ff1eb55caa20e5ab5c1bc4a07799c84
u4701 <u47-01@lgkuv.ru>
2024-05-16 18:44:14 +0300

    16.05.2024 #1

---
+++ 07052024/3.py
@@ -0,0 +1,13 @@
+a=1
+b=a
+d=a
+g=d+a
+v=b+a+g
+v=0
+
+e=b+v
+z=v+g+d
+j=e+v+z
+i=e+j+z
+
+print (i)
---
+++ 16052024_kurs/1.py
@@ -0,0 +1,46 @@
+from time import *
+ts=time()
+x=100000000
+def a3(x):
+    m=set()
+    for i in range(2,int(x**0.5)):
+        if x%i==0:
+            m.add(i)
+            m.add(x//i)
+    print (m,"\n",time()-ts)
+
+
+def a2(x):
+    m=set()
+    for i in range(2,x//2+1):
+        if x%i==0:
+            m.add(i)
+            m.add(x//i)
+    print(m, "\n", time() - ts)
+
+def a1(x):
+    m=set()
+    for i in range(2,x):
+        if x%i==0:
+            m.add(i)
+
+    print(m, "\n", time() - ts)
+
+
+def a0(x):
+    m = set()
+    x0=x
+    while x>1:
+        for i in range(2,x+1):
+            if x%i==0:
+                m.add(i)
+                m.add(x//i)
+                m.add(x0//(x//i))
+                x=x//i
+                print(x,i,m)
+                break
+    print(m, "\n", time() - ts)
+
+
+a3(x)
+
