748cd05013cb2b75e817ad73ff00d8037eb83251
bodunov78 <airfox@mail.ru>
2026-02-01 23:36:57 +0300

    2026-02-01 #2

---
+++ _2026/2026-02-01/2.py
@@ -0,0 +1,12 @@
+#7664
+
+from itertools import *
+
+def f(a,b,c,d):
+    return ( (a and b) == (not(c) ) and (b <= d) )
+
+
+tab=[(1,0,0,0),(1,0,1,0),(1,0,1,1),(1,1,0,0)]
+for p in permutations("abcd"):
+    if [f(**dict(zip(p,r))) for r in tab] ==[1,1,1,1]:
+        print (p)
